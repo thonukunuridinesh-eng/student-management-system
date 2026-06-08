a2f494d944e88e521b55866b71e67878cde98c73
thonukunuridinesh-eng <thonukunuridinesh@gmail.com>
2026-06-08 19:57:15 +0530

    Fix database issue on Vercel

--- api/index.py
+++ api/index.py
@@ -1,6 +1,15 @@
 import os
-from student_management.wsgi import application
+import django
+from django.core.management import call_command
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "student_management.settings")
 
-app = application
+django.setup()
+
+try:
+    call_command("migrate", interactive=False, verbosity=0)
+except Exception:
+    pass
+
+app = get_wsgi_application()
--- student_management/settings.py
+++ student_management/settings.py
@@ -77,10 +77,9 @@ WSGI_APPLICATION = 'student_management.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': '/tmp/db.sqlite3',
+        'NAME': '/tmp/db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
